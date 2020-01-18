d1ecb2a5addd79f3e146a1a4fb488ee1d3d3ae03
杨家馨 <578755236@qq.com>
2020-01-18 17:53:38 +0800

    注入redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { GlobalStyle } from  './style';
+import { Provider } from 'react-redux'
 import { renderRoutes } from 'react-router-config';//renderRoutes 读取路由配置转化为 Route 标签
 import { IconStyle } from './assets/iconfont/iconfont';
+import store from './store/index'
 import routes from './routes/index.js';
 import { HashRouter } from 'react-router-dom';
 console.log(1)
 function App () {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      { renderRoutes (routes) }
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        { renderRoutes (routes) }
+      </HashRouter>
+    </Provider>
   )
 }
 
