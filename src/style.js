a7d52549cfa358c103b64f1478979b0ba317227c
杨家馨 <578755236@qq.com>
2020-01-19 11:28:10 +0800

    轮播图

--- src/style.js
+++ src/style.js
@@ -1,5 +1,4 @@
 import { createGlobalStyle } from 'styled-components';
-
 export const GlobalStyle = createGlobalStyle`
 	html, body, div, span, applet, object, iframe,
 	h1, h2, h3, h4, h5, h6, p, blockquote, pre,
