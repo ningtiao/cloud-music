dfd08c025ced59c3bc25000ae99ff7829d5cf2b3
杨家馨 <578755236@qq.com>
2020-07-01 18:54:09 +0800

    推荐歌单

--- src/api/config.js
+++ src/api/config.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
-export const baseUrl = 'http://';
+export const baseUrl = 'http://172.16.1.243:3000/';
 
 const axiosInstance = axios.create({
-    baseUrl: baseUrl
+  baseURL: baseUrl
 });
 
+axios.defaults.baseURL= baseUrl
 axiosInstance.interceptors.response.use (
-    res => res.data,
-    err => {
-        console.log(err, '网络错误');
-    }
+  res => res.data,
+  err => {
+      console.log(err, '网络错误');
+  }
 );
 
 export {
