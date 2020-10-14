60af3300834395bdf7f1d5e1c0a92c404b1e7551
yjx <578755236@qq.com>
2020-10-14 16:56:37 +0800

    歌手组件

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
 import Horizen from '../../baseUI/horizen-item/index';
 import { categoryTypes, alphaTypes } from '../../api/config';
-import { NavContainer } from './style';
+import Scroll from '../../baseUI/scroll';
+import { 
+  NavContainer,
+  ListContainer,
+  List,
+  ListItem
+} from "./style";
 function Singers() {
   let [category, setCategory] = useState('');
   let [alpha, setAlpha] = useState('');
@@ -12,6 +18,34 @@ function Singers() {
 
   let handleUpdateCategory = (val) => {
     setCategory(val)
+  }
+
+  const singerList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(item => {
+    return {
+      picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
+      name: "隔壁老樊",
+      accountId: 277313426,
+    }
+  })
+
+  // 渲染函数，返回歌手列表
+  const renderSingerList = () => {
+    return (
+      <List>
+        {
+          singerList.map((item, index) => {
+            return (
+              <ListItem key={item.accountId+""+index}>
+                <div className="img_wrapper">
+                  <img src={`${item.picUrl}?parsm=300*300`} width="100%" height="100%" alt="music"></img>
+                </div>
+                <span className="name">{item.name}</span>
+              </ListItem>
+            )
+          })
+        }
+      </List>
+    )
   }
   return (
     <NavContainer>
@@ -25,6 +59,11 @@ function Singers() {
         title={"首字母:"}
         handleClick={handleUpdateAlpha}
         oldVal={alpha}></Horizen>
+      <ListContainer>
+        <Scroll>
+          { renderSingerList () }
+        </Scroll>
+      </ListContainer>
     </NavContainer>
   )
 }
